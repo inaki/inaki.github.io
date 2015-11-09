ed52b7ce7a2a75d4aec7247599746b23364ffd3d
inaki <iiaranzadi@gmail.com>
2015-11-09 12:03:46 -0800

    refactor navigation, now is just one navbar always same position

--- js/app.js
+++ js/app.js
@@ -18,42 +18,11 @@ var exec = function(){
 
 
 setTimeout('exec()', 100);
+setTimeout('$("#github").attr("class", "gray");', 100);
+setTimeout('$("#linkedin").attr("class", "gray");', 100);
 
-var goToWork = function(){
-	me.play(-3);
-	iaminaki.play(-3);
-	setTimeout('window.location.href = "/work";', 1000);
-	$( "h2" ).switchClass( "quote", "disappearing", 1000, "easeInOutQuad" );
-    $( "span" ).switchClass( "android", "disappearing", 1000, "easeInOutQuad" );
-    $( "ul.nav" ).switchClass( "fadeInUp", "fadeOutDown", 1000, "easeInOutQuad" );
-}
 
-var goToAbout = function(){
-	me.play(-3);
-	iaminaki.play(-3);
-	setTimeout('window.location.href = "/about";', 1200);
-	$( "h2" ).switchClass( "quote", "disappearing", 1000, "easeInOutQuad" );
-    $( "span" ).switchClass( "android", "disappearing", 1000, "easeInOutQuad" );
-    $( ".nav" ).switchClass( "fadeInUp", "fadeOutDown", 1000);
-}
 
-var goToResume = function(){
-	me.play(-3);
-	iaminaki.play(-3);
-	setTimeout('window.location.href = "/resume";', 1200);
-	$( "h2" ).switchClass( "quote", "disappearing", 1000, "easeInOutQuad" );
-    $( "span" ).switchClass( "android", "disappearing", 1000, "easeInOutQuad" );
-    $( ".nav" ).switchClass( "fadeInUp", "fadeOutDown", 1000);
-}
-
-var goToContact = function(){
-	me.play(-3);
-	iaminaki.play(-3);
-	setTimeout('window.location.href = "/contact";', 1200);
-	$( "h2" ).switchClass( "quote", "disappearing", 1000, "easeInOutQuad" );
-    $( "span" ).switchClass( "android", "disappearing", 1000, "easeInOutQuad" );
-    $( ".nav" ).switchClass( "fadeInUp", "fadeOutDown", 1000);
-}
 
 // If persisted then it is in the page cache, force a reload of the page.
 window.onpageshow =function(event){
--- js/resume.js
+++ js/resume.js
@@ -19,25 +19,5 @@ setTimeout('$("#github").attr("class", "gray");', 100);
 setTimeout('$("#linkedin").attr("class", "gray");', 100);
 
 
-var goToHome = function(){
-	github.play(-3);
-	setTimeout('window.location.href = "/";', 1000);
-}
-
-var goToGithub = function(){
-	github.play(-3);
-	linkedin.play(-3);
-	setTimeout('window.location.href = "http://www.github.com/inaki";', 400);
-	$( ".resume-content" ).switchClass( "fadeInUp", "fadeOutDown", 1000, "easeInOutQuad" );
-	$( ".yo" ).switchClass("fadeInDown", "fadeOut", 500, "easeInOutQuad" );
-}
-
-var goToLinkedin = function(){
-	github.play(-3);
-	linkedin.play(-3);
-	setTimeout('window.location.href = "https://www.linkedin.com/pub/imanol-aranzadi/32/533/450";', 400);
-	$( ".resume-content" ).switchClass( "fadeInUp", "fadeOutDown", 1000, "easeInOutQuad" );
-	$( ".yo" ).switchClass("fadeInDown", "fadeOut", 500, "easeInOutQuad" );
-}
 
 
