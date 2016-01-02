c02ee26e59e8d1c67cfdd43a45b3393cb846bde2
inaki <iiaranzadi@gmail.com>
2016-01-01 23:18:38 -0600

    added a new site refactored with sass and grunt tasks

---
+++ _/js/script.js
@@ -0,0 +1,1 @@
+$("#toggle").click(function(){$(this).toggleClass("active"),$("#overlay").toggleClass("open")});var exec=function(){new Vivus("me",{duration:50,onReady:function(a){}})};setTimeout("exec()",100);
